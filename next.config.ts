9fca9663bab50468deaaeaaf90b9c57b76a0bdd0
fahrilfernanda <fahrilfernanda28@gmail.com>
2026-06-25 19:10:16 +0700

    Update Smart Library

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const isProd = process.env.NODE_ENV === "production";
 
@@ -9,6 +10,10 @@ const nextConfig: NextConfig = {
 
   images: {
     unoptimized: true,
+  },
+
+  turbopack: {
+    root: path.join(__dirname),
   },
 };
 
